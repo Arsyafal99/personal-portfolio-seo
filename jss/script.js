9439919364b60e3d0117e3979cd04cb15a3eb9b3
arsyaf99 <arsyafal99@gmail.com>
2025-07-18 02:40:15 +0700

    update typing animation

--- jss/script.js
+++ jss/script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
 ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 /*==================== typed js ====================*/
 const typed = new Typed(".multiple-text", {
-  strings: ["Junior Web Developer.", "Operator LiveCam.", "User Canva."],
+  strings: ["Frontend Developer.", "SEO Enthusiast.", "Tech Learner."],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
